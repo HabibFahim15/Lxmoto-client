8863b8b49b408aa151f0e40d84bb976f5998343f
HabibFahim15 <habibfahim71@gmail.com>
2024-07-09 04:19:28 +0600

    navbar and footer are placed

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Page/Shared/Navbar/Navbar";
+import Footer from "../Page/Shared/Footer/Footer";
 
 const Main = () => {
   return (
     <div>
+      <Navbar />
+      <div className="max-w-screen-2xl mx-auto min-h-[calc(100vh-145px)]">
       <Outlet></Outlet>
+      </div>
+      <Footer />
     </div>
   );
 };
